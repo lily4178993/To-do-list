26535a4a3ec322d80c0b959f8141ed2dc65f9c62
lily4178993 <nellytelli@gmail.com>
2023-06-14 10:49:13 +0000

    Moved eslint package json to root, and fixed linter errors

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const common = require('./webpack.common');
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: "production",
-    output: {
-        filename: "main.[contentHash].js",
-        path: path.resolve(__dirname, "dist")
-    }
+  mode: 'production',
+  output: {
+    filename: 'main.[contentHash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 });
