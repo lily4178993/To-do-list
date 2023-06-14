89ed0ac2cab65503fbf40b0d4c8ab0bb5c7edd4a
lily4178993 <nellytelli@gmail.com>
2023-06-14 17:26:12 +0000

    Set an array of some simple to do tasks

---
+++ src/app/utils/data.js
@@ -0,0 +1,15 @@
+const TODO_LIST_COLLECTION = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+];
+
+export { TODO_LIST_COLLECTION as default };
