ee0eaf4a14f7076d2a154ef20e27159db8fac707
lily4178993 <nellytelli@gmail.com>
2023-06-14 09:21:32 +0000

    Installed webpack-server, Configured webpack dev and production mode

---
+++ webpack.common.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+        ],
+    },
+}
