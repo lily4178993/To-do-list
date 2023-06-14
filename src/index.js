ade155ae1d5f8251c540ac503651ba08cc424198
lily4178993 <nellytelli@gmail.com>
2023-06-14 12:17:19 +0000

    Set an array of some simple to do tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('Test OK!');
+const toDoListCollection = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+];
+console.log(toDoListCollection);
