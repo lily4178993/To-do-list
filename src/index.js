9ebee3c658c3a80a162cfe9729218ee887841a30
lily4178993 <nellytelli@gmail.com>
2023-06-15 17:34:45 +0000

    Removed all imported functions from data.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import addNewTodoItem from './app/utils/add-item.js';
-import toDoListCollection from './app/utils/data.js';
+import createTodoItem from './app/utils/display.js';
 import './style.css';
 
 const FORM_ADD_NEW_ITEM = document.querySelector('.add-item');
 FORM_ADD_NEW_ITEM.addEventListener('submit', (event) => {
   event.preventDefault();
   addNewTodoItem();
-  toDoListCollection();
+  createTodoItem();
 });
