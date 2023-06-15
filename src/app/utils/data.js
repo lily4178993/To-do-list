172beb347e47ac65b323c44ba79da8f66adfd79b
lily4178993 <nellytelli@gmail.com>
2023-06-15 12:08:58 +0000

    Added new function to add data from the local storage to an array

--- src/app/utils/data.js
+++ src/app/utils/data.js
@@ -1,3 +1,15 @@
+/**
+ * This module is used to collect all the todos tasks from local storage
+ * and store them to an array named TODO_LIST_COLLECTION
+*/
+import { getFromStorage } from './storage.js';
+
 const TODO_LIST_COLLECTION = [];
 
-export { TODO_LIST_COLLECTION as default };
+const UPDATE_COLLECTION = () => {
+  const todos = getFromStorage();
+  if (todos.length > 0) {
+    TODO_LIST_COLLECTION.push(todos);
+  }
+};
+export { UPDATE_COLLECTION as default };
