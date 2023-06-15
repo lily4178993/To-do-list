7b041031302c9afe2f8f9d25467b59fc12c8bff0
lily4178993 <nellytelli@gmail.com>
2023-06-15 17:33:38 +0000

    Stored the todos tasks from local storage inside aconstant

--- src/app/utils/data.js
+++ src/app/utils/data.js
@@ -1,15 +1,9 @@
 /**
  * This module is used to collect all the todos tasks from local storage
- * and store them to an array named TODO_LIST_COLLECTION
+ * and store them to constant named TODO_LIST_COLLECTION
 */
 import { getFromStorage } from './storage.js';
 
-const TODO_LIST_COLLECTION = [];
+const TODO_LIST_COLLECTION = getFromStorage();
 
-const UPDATE_COLLECTION = () => {
-  const todos = getFromStorage();
-  if (todos.length > 0) {
-    TODO_LIST_COLLECTION.push(todos);
-  }
-};
-export { UPDATE_COLLECTION as default };
+export default TODO_LIST_COLLECTION;
