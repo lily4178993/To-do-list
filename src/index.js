a707bbc4dad17155912f9ea63c05c39376468bb4
lily4178993 <nellytelli@gmail.com>
2023-06-15 12:09:47 +0000

    Imported new modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import ITERATE_TODO_LIST from './app/utils/display.js';
+import addNewTodoItem from './app/utils/add-item.js';
+import toDoListCollection from './app/utils/data.js';
 import './style.css';
 
-ITERATE_TODO_LIST();
+const FORM_ADD_NEW_ITEM = document.querySelector('.add-item');
+FORM_ADD_NEW_ITEM.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addNewTodoItem();
+  toDoListCollection();
+});
